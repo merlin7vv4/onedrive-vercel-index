4c73eb54a1c032bb38fb7f1e89c2e3c7594c60a3
Merlin <68115291+merlin7vv4@users.noreply.github.com>
2023-03-12 18:39:16 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,11 +20,11 @@ module.exports = {
   title: "merlin",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/Family Guy',
+  baseDirectory: '/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
-  maxItems: 100,
+  maxItems: 200,
 
   // [OPTIONAL] We use Google Fonts natively for font customisations.
   // You can check and generate the required links and names at https://fonts.google.com.

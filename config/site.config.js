43601d1e6b997f70f902221a39105b93620a55c3
Merlin <68115291+merlin7vv4@users.noreply.github.com>
2023-03-12 13:42:42 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -41,7 +41,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/1234', '/🥟 Some test files/Protected route'],
+  protectedRoutes: ['/1234', '/Private'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:truonglong17120@gmail.com',
@@ -49,12 +49,12 @@ module.exports = {
   // [OPTIONAL] This is an array of names and links for setting your social information and links.
   // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
   // you provide. See the documentation for details.
-  links: [
-    {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
-    },
-  ],
+//   links: [
+//     {
+//       name: 'GitHub',
+//       link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+//     },
+//   ],
 
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
